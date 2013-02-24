741714a794e95f4880998a3898a8ee5523889b72
Phy <git@phy25.com>
2013-02-24 16:12:39 +0800

    The whole index page is reseted now.

--- index.js
+++ index.js
@@ -4,24 +4,38 @@
 var exam_list = {};
 var $kaoshi = $('#kaoshi');
 $kaoshi.find('option').each(function(){
-	exam_list[$(this).attr('value')] = $(this).text();
+	exam_list[$(this).attr('value')] = $(this).text().replace(/(\s)/g,'');
 });
 //for(var i in a){console.log(i)}
-console.log(exam_list);
+//<label class="radio"><input type="radio" name="optionsRadios" id="optionsRadios1" value="option1" checked>Option one is this and that—be sure to include why it's great</label>
 
-var $f = $('form:first').addClass('form-horizontal').html('');
+
+var $f = $('form:first'), update_status_t = $f.find('b').text().replace(/(\s)/g,''), orig_announcement_t = $f.next().text().replace(/(\s)(\s)+/g,'$1');
+
+// Fetching ended, now start structure fill
 
 var $fa = $('<div class="control-group"><label class="control-label" for="xuehao">学号</label><div class="controls"><input type="number" id="xuehao" name="xuehao" placeholder="学号" required="required" min="10101" max="32100" /></div></div>');
 $fa.after('<div class="control-group"><label class="control-label" for="inputPassword">密码</label><div class="controls"><input type="text" id="password" name="password" placeholder="密码" required="required" pattern="^(\\d{15}$|^\\d{18}$|^\\d{17}(\\d|X|x))$" /></div></div>');
-$fa.after('<div class="control-group" id="exam-control"><label class="control-label" for="kaoshi">考试</label><div class="controls"></div></div>');
-$fa.after('<div class="control-group"><div class="controls"><input type="submit" class="btn" value="查询" /></div></div>');
+$fa.after('<div class="control-group" id="exam-control"><label class="control-label" for="kaoshi">考试</label><div class="controls"><span class="help-inline">请先输入学号</span></div></div>');
+$fa.after('<div class="control-group"><div class="controls"><input type="submit" class="btn btn-primary" value="查询" /></div></div>');
 
-$f.append($fa);
+$f.addClass('form-horizontal').html('').append($fa);
 
-$('#exam-control .controls').append($kaoshi);
+var $container = $('<div class="container-fluid" />').appendTo($('<body />').replaceAll('body'));
+
+var $append = $('<h1>金中成绩查询</h1>')
+	.after($f)
+	.after('<h2>教务处通知</h2><div id="orig-announcement"></div>');
+
+$container.append($append);
+
+// Structure fill ended, now content fill
+
+$('#orig-announcement').append('<div class="alert alert-info">'+update_status_t+'</div><p>'+orig_announcement_t.replace(/(\n|\r)/g,'</p><p>')+'</p>');
 
 $("#xuehao").change(function(){
 	var no = $(this).val();
+	$kaoshi.detach();
 	if(no.indexOf('1') == 0){
 		$kaoshi.find('option').each(function(){
 			if(this.innerText.indexOf('高一') == -1){
@@ -46,14 +60,5 @@ $("#xuehao").change(function(){
 		$kaoshi.children().show();
 		$kaoshi.find('option:contains(高三):first')[0].selected = true;
 	}
+	$('#exam-control .controls').empty().append($kaoshi);
 });
-
-
-$('div.Section1>div>table>tbody>:first-child>td>:first-child>table>tbody>:nth-child(5)').append('<td rowspan="1" style="padding:0cm 0cm 0cm 0cm" background="images/search_r4_c4.jpg"><p class="MsoNormal" style="line-height:150%;word-break:break-all"><span style="font-size:9.0pt;line-height:150%"><span lang="EN-US"><o:p></o:p></span></span></p></td>');
-//
-
-/*
-  
-  
-  
-*/
