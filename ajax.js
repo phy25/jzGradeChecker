5e28ab24b55fc66d254d1faa30a756ee70a142bb
Phy <git@phy25.com>
2013-05-12 00:33:58 +0800

    完成可用的 Ajax 获取成绩模块。

--- ajax.js
+++ ajax.js
@@ -7,7 +7,7 @@ if(!jzgc) var jzgc = {};
 
 jzgc.ajax = {
 	// data: {xuehao, password, kaoshi}
-	getExamResult: function(data, successCallback, failCallback = null){
+	getExamResult: function(data, successCallback, failCallback){
 		if(localStorage && localStorage['stu_arr_0']){
 			var stu_arr = [(localStorage['stu_arr_0'] || '').split(';')];
 			if(!data.xuehao){
@@ -20,7 +20,7 @@ jzgc.ajax = {
 		if(!data.kaoshi) return false;
 		
 		$.ajax({
-			url: 'search.asp',
+			url: 'http://jszx.stedu.net/jszxcj/search.asp',
 			type: 'POST',
 			data: data,
 			dataType: 'html',
@@ -28,12 +28,43 @@ jzgc.ajax = {
 				if(typeof failCallback = 'function') failCallback(textStatus, errorThrown);
 			},
 			success: function(data) {
+				if(data.indexOf('3;url=Search.htm') != -1){
+					if(typeof failCallback == 'function') failCallback('certError', data);
+					return false;
+				}
 				// Thanks to jQuery.load(): removing the scripts
 				// to avoid any 'Permission Denied' errors in IE
-				var $("<div>").append(data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, "")).find('table:eq(3)');
+				var $d = $("<div>").append(data.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, ""));
+				$d = $d.find('table:eq(3)');
 
-				if(typeof successCallback = 'function') successCallback();
+				if(typeof successCallback == 'function') successCallback(jzgc.ajax.getTableArr($d));
 			}
 		});
+	},
+	getTableArr: function($Elem){
+		var r = {'thead':[], 'tbody':[]}, $h, $b;
+		$h = $Elem.find('thead tr:first');
+		if(!$h.length){
+			$h = $Elem.find('tbody tr:first');
+			$b = $Elem.find('tbody tr:not(:first)');
+		}else{
+			$b = $Elem.find('tbody tr');
+		}
+		$h.find('th, td').each(function(i){
+			if(i > 0){
+				r.thead[r.thead.length] = $.text(this).replace(/(\s)/g,'');
+			}
+		});
+		
+		$b.each(function(){
+			r.tbody[r.tbody.length] = {};
+			var td = r.tbody[r.tbody.length-1], $td = $(this).find('td');
+			td.subject = $td.eq(0).text().replace(/(\s)/g,'');
+			$td.not(':first').each(function(i){
+				td[r.thead[i]] = $(this).text().replace(/(\s)/g,'');
+			});
+		});
+
+		return r;
 	}
 };
