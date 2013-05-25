335e6576f48079e5db6d5452f7f4faece0462b51
Phy <git@phy25.com>
2013-05-25 16:33:34 +0800

    增加扩展安装时的 GA 统计。增加 Bootstrap Buttons 样式并应用。

--- chrome-extension/install.js
+++ chrome-extension/install.js
@@ -1,13 +1,19 @@
+function $id(id){
+	return document.getElementById(id) || {};
+}
+
 if(window.chrome){
 	var wsurl = 'https://chrome.google.com/webstore/detail/gghlnllohkclgmeefgaenonjjlndceap';
-	document.getElementById('non-chrome-notice').style.display = 'none';
+	$id('non-chrome-notice').style.display = 'none';
 	if(chrome.webstore && chrome.webstore.install){
-		document.getElementById('install-link-quick').addEventListener('click', function(e){e.preventDefault();});
-		document.getElementById('install-link-crws').addEventListener('click', function(e){e.preventDefault();});
-		document.getElementById('install-link-quick').addEventListener('click', function(){
+		// $id('install-link-quick').addEventListener('click', function(e){e.preventDefault();});
+		// $id('install-link-crws').addEventListener('click', function(e){e.preventDefault();});
+		$id('install-link-quick').addEventListener('click', function(e){
+			e.preventDefault();
 			var error = function(t){
 				if(t == 'User cancelled install') return false;
-				if(confirm("网上应用店安装失败"+(t?("（"+t+"）\n"):"")+"。建议您在官方安装扩展，需要继续吗？")){
+				_gaq.push(['_trackEvent', 'jzGradeChecker', 'WebstoreFailure', t]);
+				if(confirm("要在扩展官方下载扩展（推荐操作）吗？"+(t?("\n（在网上应用店安装失败："+t+"）"):""))){
 					location = 'latest.crx';
 					lightbox_init();
 				}
@@ -15,40 +21,53 @@ if(window.chrome){
 			try{
 				chrome.webstore.install(wsurl,
 					function(){ // for Chrome
-						location = '../';
+						trackLinkEvent('../', 'jzGradeChecker', 'WebstoreSuccess');
 					}, error);
 			}catch(e){// for fxxking 360
 				error(e.description);
 			}
 			return false;
 		});
-		document.getElementById('install-link-crws').addEventListener('click', function(){
+		$id('install-link-crws').addEventListener('click', function(e){
+			e.preventDefault();
 			var error = function(t){
 				if(t == 'User cancelled install') return false;
-				if(confirm('错误：'+t+"\n是否到 Chrome 网上应用店页面安装？")){
+				_gaq.push(['_trackEvent', 'jzGradeChecker', 'WebstoreFailure', t]);
+				if(confirm('错误：'+t+"\n要到 Chrome 网上应用店页面安装吗？")){
 					location = wsurl;
 				}
 			};
 			try{
 				chrome.webstore.install(wsurl,
 					function(){ // for Chrome
-						location = '../';
+						trackLinkEvent('../', 'jzGradeChecker', 'WebstoreSuccess');
 					}, error);
 			}catch(e){// for fxxking 360
 				error(e.description);
 			}
 			return false;
 		});
-		document.getElementById('install-link-direct').addEventListener('click', function(){
+		$id('install-link-direct').addEventListener('click', function(){
 			lightbox_init();
 		});
 		function lightbox_init(){
-			document.getElementById('shadowing').style.display = 'block';
-			document.getElementById('box').style.display = 'block';
-			document.getElementById('shadowing').addEventListener('click', function(){
-				document.getElementById('shadowing').style.display = 'none';
-				document.getElementById('box').style.display = 'none';
+			_gaq.push(['_trackEvent', 'jzGradeChecker', 'Download', 'direct']);
+			$id('shadowing').style.display = 'block';
+			$id('box').style.display = 'block';
+			$id('shadowing').addEventListener('click', function(){
+				$id('shadowing').style.display = 'none';
+				$id('box').style.display = 'none';
 			});
 		}
+		function trackLinkEvent(loc, cat, act, attr, val){
+			_gaq.push(['_trackEvent', cat, act, attr, val]);
+			setTimeout(function() {
+				document.location.href = loc;
+			}, 100);
+		}
+		$id('install-direct-done').addEventListener('click', function(){
+			trackLinkEvent(this.href, 'jzGradeChecker', 'UIDownloadLightboxContinue');
+			return false;
+		});
 	}
 }
