8858f6872cd9ec194bf499bc55cdeaf43bcc290b
Phy <git@phy25.com>
2013-07-26 13:16:30 +0800

    发 0.7.3 啦。

--- func-common.js
+++ func-common.js
@@ -5,7 +5,7 @@ Common functions
 if(!jzgc) var jzgc = {};
 
 jzgc.config = {
-	version: ['chrome-extension', '0.7.2', 'http://github.phy25.com/jzGradeChecker/'],
+	version: ['chrome-extension', '0.7.3', 'http://github.phy25.com/jzGradeChecker/'],
 	// A sequential exam list **TODO**
 	examListOrganized: [],
 	konamiCode: "' or ''='"
