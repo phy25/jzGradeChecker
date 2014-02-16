4cf95624520f0ae02c57c66409544d1e6af5a64f
Phy <git@phy25.com>
2014-02-16 11:27:42 +0800

    修复“原学号”的读取。

--- ce-result.js
+++ ce-result.js
@@ -35,7 +35,7 @@ function startExecution(){
 		// Save the time to give hints on index.js
 		jzgc.user.attrSave('lastChecked', +new Date());
 		jzgc.user.attrSave('name', resultData.meta['姓名']);
-		if(resultData.meta['新学号'] && resultData.meta['新学号'].substr(1, 4) != resultData.meta['学号'].substr(1, 4)){
+		if(resultData.meta['新学号'] && resultData.meta['新学号'].substr(1, 4) != resultData.meta['原学号'].substr(1, 4)){
 			jzgc.user.attrSave('newxuehao', resultData.meta['新学号']);
 		}else{
 			jzgc.user.attrClear('newxuehao');
