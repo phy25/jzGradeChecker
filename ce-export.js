9b11b313f48354b723885ed4722cdf00c9c39e89
phy25 <git@phy25.com>
2015-07-14 12:19:08 +0800

    export 优化。Ajax 部分，增加把无用的 img 的 src 移除的功能（避免 console 出错 404）。

--- ce-export.js
+++ ce-export.js
@@ -2,6 +2,17 @@
 Export page handler for chrome extension
 */
 $(function(){
+	if(jzgc.config && jzgc.config.examList){
+
+	}else{
+		jzgc.ajax.getExamList(
+			function(r){
+				jzgc.config.examList = r.examList;
+			}, function(t, e){
+				alert('无法加载考试列表，故无法继续导出。');
+			}
+		);
+	}
 	$('#exam-selector ul.nav a').click(function(e){
 		if(!$(e.target).is('input')) e.preventDefault();
 		$(this).tab('show');
